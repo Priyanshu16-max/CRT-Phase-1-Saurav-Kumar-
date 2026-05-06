978c820fddcf10caf9c03493000312b86f318d85
Priyanshu16-max <priyanshupareek925@gmail.com>
2026-05-06 09:52:39 +0530

    hello

--- Day-6/basics.cpp
+++ Day-6/basics.cpp
@@ -5,15 +5,28 @@
 // };
 
 
-#include<iostream>
+#include <iostream>
 using namespace std;
-
-class Student{
-    public:
+class Student
+{
+public:
+    // Data members
     string name;
     int rollno;
-    void display(){
-        cout<<"Name: "<<name<<endl;
-        cout<<"rollno: "<<rollno<<endl;
+
+    // member function
+    void display()
+    {
+        cout << "Name:" << name << endl;
+        cout << "Roll No:" << rollno << endl;
     }
 };
+int main()
+{
+    Student s1;
+    s1.name = "Saurav";
+    s1.rollno = 18;
+    s1.display();
+
+    return 0;
+}
