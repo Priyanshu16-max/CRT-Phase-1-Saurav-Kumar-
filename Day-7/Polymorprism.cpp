26db5e9419ab3228ebb09c0223fe6d8061d45423
Priyanshu16-max <priyanshupareek925@gmail.com>
2026-05-07 11:42:28 +0530

    hello

---
+++ Day-7/Polymorprism.cpp
@@ -0,0 +1,9 @@
+//polymorprism:- polymorprism is a feature of OOP that allows objects of different classes to be treated as objects of a common base class. 
+//It is achieved through inheritance and virtual functions.
+// Polymorphism allows for dynamic method binding, which means that the method that gets called is determined at runtime based on the type of the object being referred to, rather than the type of the reference variable.
+// compile time:- function overloading and operator overloading
+// run time:- virtual functions and pure virtual functions
+
+
+
+
