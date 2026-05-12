ae8b9b321f57a4dbedb2ce30c4ad1b6765a0b0db
Priyanshu16-max <priyanshupareek925@gmail.com>
2026-05-12 08:06:37 +0530

    Normal Commit

--- Day-9/reverse-array.cpp
+++ Day-9/reverse-array.cpp
@@ -5,7 +5,7 @@ void ReverseArray(int nums[], int n)
 {
     int i = 0;
     int j = n - 1;
-    while(i < j){
+while(i < j ){
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
