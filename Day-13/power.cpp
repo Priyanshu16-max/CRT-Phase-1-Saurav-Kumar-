756da5c85fed439807566cd883e29d4d5ad43d04
Priyanshu16-max <priyanshupareek925@gmail.com>
2026-05-13 09:47:58 +0530

    Normal Commit

--- Day-13/power.cpp
+++ Day-13/power.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 bool isPow(int x)
 {
-    if (x == 0)
+    if (x == 0)                               
         return false;
     if (x == 1)
         return true;
-    return (x % 2 == 0) && isPow(x / 2);
+    return (x % 2 == 0) && isPow(x / 2);           // % 3 == 0 && isPow(x / 3) for 3 ^ n
 }
 
 int main()
